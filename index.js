5e2d24faa54620667aa6a5cac0e6e7ed6fd57fd1
Douglas Alves <douglas.expalves@gmail.com>
2017-08-17 23:37:29 -0300

    comecei a fazer validações através do Joi, rotas de teste criadas

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 'use strict';
 
 const Hapi = require('hapi');
+const Joi = require('joi');
 const app = new Hapi.Server();
 let nome = 'User';
 
@@ -23,21 +24,52 @@ app.route({
   }
 });
 
-server.register(require('inert'), err => {
+app.route({
+  method : 'POST',
+  path : '/api/v1/teste/{name}',
+  handler: function(request, reply) {
+    reply({
+      reply : `${request.params.name} ${request.payload.surname}`
+    });
+  }, config : {
+    validate : {
+      params : {
+        name: Joi.string().required()
+      },
+      payload : Joi.object({
+        surname : Joi.string().min(3).max(40).required()
+      })
+    }
+  }
+});
+
+
+app.route({
+  method : 'PUT',
+  path : '/teste/:id',
+  handler: function(request, reply) {
+
+  }
+});
+
+app.register(require('inert'), err => {
   if (err) {
     throw err;
   }
 
-  server.route({
+  app.route({
     method: 'GET',
     path: '/hello',
     handler: function(request, reply) {
-      reply.file('');
+      reply.file('src/views/index.html', (err) =>{
+        if (err) return console.log(err);
+
+      });
     }
   });
 });
 
-app.start(err => {
-  if (err) return err;
+app.start((err) => {
+  if (err) return console.log(err);
   console.log('Servidor iniciado com sucesso na %s', app.info.uri);
 });
