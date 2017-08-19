92c82daed576ee7a9a75736f3c772dfb7598a702
Douglas Alves <douglas.expalves@gmail.com>
2017-08-18 23:47:07 -0300

    introdução do hapi-router e inicio da modularização

--- index.js
+++ index.js
@@ -2,74 +2,20 @@
 
 const Hapi = require('hapi');
 const Joi = require('joi');
-const app = new Hapi.Server();
-let nome = 'User';
+const server = new Hapi.Server();
+server.connection({ port: 3000, host: '0.0.0.0' });
 
-app.connection({ port: 3000, host: '0.0.0.0' });
-
-app.route({
-  method: 'GET',
-  path: '/',
-  handler: (request, reply) =>
-    reply({
-      message : 'Hello World'
-    })
-});
-
-app.route({
-  method: 'GET',
-  path: `/${nome}`,
-  handler: function(request, reply) {
-    reply({ message: `Hello, ${nome}!` });
+server.register({
+  register: require('hapi-router'),
+  options: {
+   routes: 'src/**/routes.js'
   }
-});
-
-app.route({
-  method : 'POST',
-  path : '/api/v1/teste/{name}',
-  handler: function(request, reply) {
-    reply({
-      reply : `${request.params.name} ${request.payload.surname}`
-    });
-  }, config : {
-    validate : {
-      params : {
-        name: Joi.string().required()
-      },
-      payload : Joi.object({
-        surname : Joi.string().min(3).max(40).required()
-      })
-    }
-  }
-});
-
-
-app.route({
-  method : 'PUT',
-  path : '/teste/:id',
-  handler: function(request, reply) {
-
-  }
-});
-
-app.register(require('inert'), err => {
-  if (err) {
-    throw err;
-  }
-
-  app.route({
-    method: 'GET',
-    path: '/hello',
-    handler: function(request, reply) {
-      reply.file('src/views/index.html', (err) =>{
-        if (err) return console.log(err);
-
-      });
-    }
-  });
-});
-
-app.start((err) => {
-  if (err) return console.log(err);
-  console.log('Servidor iniciado com sucesso na %s', app.info.uri);
-});
+ }).then(function() {
+    return serverStart()
+ })
+ function serverStart() {
+  server.start(err => {
+    if (err) throw err
+    console.log('Servidor iniciando em %s', server.info.uri)
+  })
+ }
