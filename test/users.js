482b562f0f04ec7ac3302a4efe4c9afd34ced408
Douglas Alves <douglas.expalves@gmail.com>
2017-08-20 23:49:51 -0300

    primeiro teste com lab e code

---
+++ test/users.js
@@ -0,0 +1,23 @@
+'use strict';
+
+const lab = (exports.lab = require('lab').script());
+const { expect } = require('code');
+const server = require('../index');
+
+lab.describe('Testes da resource user', () => {
+  lab.test('Deve retornar com {sucess:true} ', () => {
+    return server
+      .inject({
+        method: 'POST',
+        url: '/api/v1/users/',
+        payload: {
+          username: 'douglas',
+          password: 'pass'
+        }
+      })
+      .then(res => {
+        expect(res.statusCode).to.equal(200);
+        expect(res.result).to.equal({ sucess: true });
+      });
+  });
+});
