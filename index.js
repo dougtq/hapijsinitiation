bedff207acdab37e2cef5144de23669627ee5b2c
Douglas Alves <douglas.expalves@gmail.com>
2017-08-21 02:07:46 -0300

    modularização

--- index.js
+++ index.js
@@ -1,25 +1,14 @@
-'use strict';
+const server = require('./src/server');
 
-const Hapi = require('hapi');
-const Joi = require('joi');
-const server = new Hapi.Server();
-server.connection({ port: 3000, host: '0.0.0.0' });
-
-server.register({
-  register: require('hapi-router'),
-  options: {
-   routes: 'src/**/routes.js'
-  }
- }).then(function() {
-    return serverStart()
- })
- function serverStart() {
-  if (require.main === module){
-    server.start(err => {
-      if (err) throw err
-      console.log('Servidor iniciando em %s', server.info.uri)
-    })
-  }else{
-    module.exports = server;
-  }
- }
+(function serverStart() {
+  // if (require.main === module){
+  server.start(err => {
+    if (err) throw err;
+    console.log('Servidor iniciando em %s', server.info.uri);
+  });
+  
+  // }else{
+  //   module.exports = server;
+  // }
+  //module.exports = server;
+}())
