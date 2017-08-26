22402d45ccfa6406ea8296888b61cc4bde2dc239
Douglas Alves <douglas.expalves@gmail.com>
2017-08-26 00:25:48 -0300

    async() e await, e injeção de teste de falha

--- test/users.js
+++ test/users.js
@@ -5,19 +5,31 @@ const { expect } = require('code');
 const server = require('../src/server');
 
 lab.describe('Testes da resource user', () => {
-  lab.test('Deve retornar com {sucess:true} ', () => {
-    return server
-      .inject({
-        method: 'POST',
-        url: '/api/v1/users/',
-        payload: {
-          username: 'douglas',
-          password: 'pass'
-        }
-      })
-      .then(res => {
-        expect(res.statusCode).to.equal(200);
-        expect(res.result).to.equal({ sucess: true });
-      });
+  lab.test('Deve retornar com {sucess:true} ', async () => {
+    const res = await server.inject({
+      method: 'POST',
+      url: '/api/v1/users/',
+      payload: {
+        username: 'douglas',
+        password: 'pass'
+      }
+    });
+
+    expect(res.statusCode).to.equal(200);
+    expect(res.result).to.equal({ sucess: true });
   });
+});
+
+lab.test('Deve retornar com { msg : Usuário já existente} ', async () => {
+  const res = await server.inject({
+    method: 'POST',
+    url: '/api/v1/users/',
+    payload: {
+      username: 'doug',
+      password: 'pass'
+    }
+  }).then(res => {
+    expect(res.statusCode).to.equal(422);
+    expect(res.result.message).to.equal('Usuário já existente');
+  })
 });
